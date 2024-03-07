c42fd205318288a10fa43a720f4e41ea315e3e12
ChristianMaglalang6910 <christian.maglalang@dcmail.ca>
2024-03-06 23:32:54 -0500

    Added descriptions

--- Assignment4/Assignment4Part1/main.js
+++ Assignment4/Assignment4Part1/main.js
@@ -3,7 +3,7 @@
     Student Number: 100911001
 	File: INFT 1206 Assignment 4 Part 1
 	Date: March 22, 2024
-	Description:
+	Description: Create a silly story generator using the given html and javascript.
 */
 
 
