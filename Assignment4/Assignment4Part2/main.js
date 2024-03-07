94e1aaab9f6ea74e93870b64f4ad4cbf0f27b037
ChristianMaglalang6910 <christian.maglalang@dcmail.ca>
2024-03-07 00:06:55 -0500

    Added html, js, and css files to assignment 4

---
+++ Assignment4/Assignment4Part2/main.js
@@ -0,0 +1,26 @@
+/*
+	Name: Christian Maglalang
+    Student Number: 100911001
+	File: INFT 1206 Assignment 4 Part 2
+	Date: March 22, 2024
+	Description: 
+*/
+
+const displayedImage = document.querySelector('.displayed-img');
+const thumbBar = document.querySelector('.thumb-bar');
+
+const btn = document.querySelector('button');
+const overlay = document.querySelector('.overlay');
+
+/* Declaring the array of image filenames */
+
+/* Declaring the alternative text for each image file */
+
+/* Looping through images */
+
+const newImage = document.createElement('img');
+newImage.setAttribute('src', xxx);
+newImage.setAttribute('alt', xxx);
+thumbBar.appendChild(newImage);
+
+/* Wiring up the Darken/Lighten button */
---
+++ Assignment4/Assignment4Part3/main.js
@@ -0,0 +1,28 @@
+/*
+	Name: Christian Maglalang
+    Student Number: 100911001
+	File: INFT 1206 Assignment 4 Part 3
+	Date: March 22, 2024
+	Description:
+*/
+
+// setup canvas
+
+const canvas = document.querySelector('canvas');
+const ctx = canvas.getContext('2d');
+
+const width = (canvas.width = window.innerWidth);
+const height = (canvas.height = window.innerHeight);
+
+// function to generate random number
+
+function random(min, max) {
+  const num = Math.floor(Math.random() * (max - min + 1)) + min;
+  return num;
+}
+
+// function to generate random color
+
+function randomRGB() {
+  return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
