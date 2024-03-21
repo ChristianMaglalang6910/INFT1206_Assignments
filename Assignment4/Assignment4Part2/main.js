44147d5991cab50af30e777fe29876a7588c35c4
ChristianMaglalang6910 <christian.maglalang@dcmail.ca>
2024-03-20 22:46:07 -0400

    Declared array of image filenames and declare alternative texts for each image file

--- Assignment4/Assignment4Part2/main.js
+++ Assignment4/Assignment4Part2/main.js
@@ -13,14 +13,17 @@ const btn = document.querySelector('button');
 const overlay = document.querySelector('.overlay');
 
 /* Declaring the array of image filenames */
+const images = [pic1.jpg, pic2.jpg, pic3.jpg, pic4.jpg, pic5.jpg]
 
 /* Declaring the alternative text for each image file */
+const imageText = {
+    pic1: "Image 1",
+    pic2: "Image 2",
+    pic3: "Image 3",
+    pic4: "Image 4",
+	pic5: "Image 5"
+};
 
 /* Looping through images */
-
-const newImage = document.createElement('img');
-newImage.setAttribute('src', xxx);
-newImage.setAttribute('alt', xxx);
-thumbBar.appendChild(newImage);
 
 /* Wiring up the Darken/Lighten button */
